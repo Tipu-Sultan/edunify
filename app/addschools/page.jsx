fadf5b8f7bcd45af67c66b87e391f9df7eb70114
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-11-24 02:03:28 +0530

    attempt

--- app/addschools/page.jsx
+++ app/addschools/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import axios from 'axios';
@@ -184,3 +185,4 @@ const Page = () => {
 };
 
 export default Page;
+
