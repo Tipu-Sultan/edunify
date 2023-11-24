51b70be93dfb3e9c5bf4228e4ad940082652786d
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-11-25 02:25:13 +0530

    mmsmkmks

--- pages/api/addschool.js
+++ pages/api/addschool.js
@@ -1,11 +1,12 @@
 // pages/api/addschool.js
 import { connectDB } from '../../utils/db';
-import {School} from '../../models/school';
+import {School} from '../../models/school'; // Assuming School is the default export
 import multer from 'multer';
+import nc from 'next-connect';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public/images"); // Uploads folder where files will be stored
+    cb(null, "public/images"); // Uploads folder where files will be stored
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname); // Unique filename
@@ -14,52 +15,47 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+const handler = nc();
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      // Connect to the database
-      await connectDB();
-      
-      // Handle file upload
-      upload.single('file')(req, res, async (err) => {
-        if (err) {
-          console.error('Error uploading file:', err);
-          return res.status(500).json({ success: false, error: 'Internal Server Error file',Name:req.body.name });
-        }
-        // Create a new school instance
-        const school = new School({
-          name: req.body.name,
-          address: req.body.address,
-          city: req.body.city,
-          state: req.body.state,
-          contact: req.body.contact,
-          email: req.body.email,
-          image: req.file.filename,
-        });
+handler.use(upload.single('file'));
 
-        // Save the school to the database
-        const savedSchool = await school.save();
+handler.post(async (req, res) => {
+  try {
+    // Connect to the database
+    await connectDB();
 
-        // Send response
-        res.status(201).json({
-          success: true,
-          data: {message: 'School added successfully' },
-        });
-      });
-    } catch (error) {
-      console.error('Error adding school:', error);
-      res.status(500).json({ success: false, error: 'Internal Server Error all' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+    // Assuming you have req.file and req.body available after multer middleware
+    const { name, address, city, state, contact, email } = req.body;
+
+    // Create a new school instance
+    const school = new School({
+      name,
+      address,
+      city,
+      state,
+      contact,
+      email,
+      image: req.file.filename,
+    });
+
+    // Save the school to the database
+    const savedSchool = await school.save();
+
+    // Send response
+    res.status(201).json({
+      success: true,
+      data: {
+        message: "School added successfully",
+      },
+    });
+  } catch (error) {
+    console.error('Error adding school:', error);
+    res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
-}
+});
+
+export default handler;
+
 
 
 
