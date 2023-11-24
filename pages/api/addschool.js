fb4d790a3edd791c21df6f0aac9f34e64fe0b009
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-11-25 02:45:19 +0530

    again check

--- pages/api/addschool.js
+++ pages/api/addschool.js
@@ -1,12 +1,10 @@
-// pages/api/addschool.js
 import { connectDB } from '../../utils/db';
-import {School} from '../../models/school'; // Assuming School is the default export
+import {School} from '../../models/school';
 import multer from 'multer';
-import {createRouter} from 'next-connect';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images"); // Uploads folder where files will be stored
+    cb(null, "./public/images"); // Uploads folder where files will be stored
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname); // Unique filename
@@ -15,46 +13,53 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-const router = createRouter();
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
-router.use(upload.single('file'));
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      // Connect to the database
+      await connectDB();
 
-router.post(async (req, res) => {
-  try {
-    // Connect to the database
-    await connectDB();
+      // Handle file upload
+      upload.single('file')(req, res, async (err) => {
+        if (err) {
+          console.error('Error uploading file:', err);
+          return res.status(500).json({ success: false, error: 'Internal Server Error' });
+        }
 
-    // Assuming you have req.file and req.body available after multer middleware
-    const { name, address, city, state, contact, email } = req.body;
+        // Create a new school instance
+        const school = new School({
+          name: req.body.name,
+          address: req.body.address,
+          city: req.body.city,
+          state: req.body.state,
+          contact: req.body.contact,
+          email: req.body.email,
+          image: req.file.filename,
+        });
 
-    // Create a new school instance
-    const school = new School({
-      name,
-      address,
-      city,
-      state,
-      contact,
-      email,
-      image: req.files.filename,
-    });
+        // Save the school to the database
+        const savedSchool = await school.save();
 
-    // Save the school to the database
-    const savedSchool = await school.save();
-
-    // Send response
-    res.status(201).json({
-      success: true,
-      data: {
-        message: "School added successfully",
-      },
-    });
-  } catch (error) {
-    console.error('Error adding school:', error);
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
+        // Send response
+        res.status(201).json({
+          success: true,
+          data: { id: savedSchool._id, ...req.body, picture: req.file.filename, message: 'School added successfully' },
+        });
+      });
+    } catch (error) {
+      console.error('Error adding school:', error);
+      res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
-});
-
-export default router.handler();
+}
 
 
 
