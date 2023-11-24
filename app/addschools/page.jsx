3d685cf5475f3f19d73bb617d4232e3f4f1b1ec5
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-11-24 13:41:23 +0530

    fix bugs

--- app/addschools/page.jsx
+++ app/addschools/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from 'react';
 
-const AddSchoolForm = () => {
+const page = () => {
   const [formData, setFormData] = useState({
     name: '',
     address: '',
