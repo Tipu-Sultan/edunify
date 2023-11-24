805ba5d55add69c22241c58b89d0e0fd1c7f8042
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-11-24 13:40:18 +0530

    recheck again

--- app/addschools/page.jsx
+++ app/addschools/page.jsx
@@ -1,66 +1,96 @@
 "use client";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "react-toastify";
-import axios from 'axios';
+import React, { useState } from 'react';
 
-const page = () => {
-  const [loading, setLoading] = useState(false);
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    defaultValues: {
-      name: '',
-      address: '',
-      city: '',
-      state: '',
-      contact: '',
-      email: '',
-      picture: '',
-    }
+const AddSchoolForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    address: '',
+    city: '',
+    state: '',
+    contact: '',
+    email: '',
+    picture: null,
   });
 
-  const onSubmit = async (data) => {
-    const formData = new FormData();
-    formData.append("picture", data.picture[0]);
-    formData.append("name", data.name);
-    formData.append("address", data.address);
-    formData.append("city", data.city);
-    formData.append("state", data.state);
-    formData.append("contact", data.contact);
-    formData.append("email", data.email);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value, type, files } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'file' ? files[0] : value,
+    }));
+  };
+
+  const validateContact = (contact) => /^[0-9]{10}$/.test(contact);
+
+  const validateEmail = (email) =>
+    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+
+  const validateFileType = (file) => {
+    const allowedFileTypes = ['.jpg', '.jpeg', '.png'];
+    const fileExtension = file.name.split('.').pop().toLowerCase();
+    return allowedFileTypes.includes(`.${fileExtension}`);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validate form data here if needed
+    if (
+      !formData.name ||
+      !formData.address ||
+      !formData.city ||
+      !formData.state ||
+      !validateContact(formData.contact) ||
+      !validateEmail(formData.email) ||
+      (formData.picture && !validateFileType(formData.picture))
+    ) {
+      alert('Please fill in all the required fields and provide valid data.');
+      return;
+    }
+
+    // Simulate loading
     setLoading(true);
-  
+
+    // Your API request code goes here
     try {
-      const response = await axios.post("https://edunify.vercel.app/api/addschool", formData);
-  
-      if (response.status === 200) {
-        const result = response.data;
-        console.log("Data submitted successfully:", result.data);
-        toast.success(result.data.message);
-        setLoading(false);
-      } else {
-        console.error("Failed to submit data:", response.statusText);
-        toast.error("Failed to submit data");
-        setLoading(false);
-      }
+      // Example using fetch
+      const apiUrl = '/api/addschool';
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        body: new FormData(e.target),
+      });
+
+      const data = await response.json();
+      console.log('Data submitted successfully:', data);
+
+      // Reset the form after successful submission
+      setFormData({
+        name: '',
+        address: '',
+        city: '',
+        state: '',
+        contact: '',
+        email: '',
+        picture: null,
+      });
     } catch (error) {
-      console.error("Error submitting data:", error);
-      toast.error("Error submitting data");
+      console.error('Error submitting data:', error);
+    } finally {
       setLoading(false);
     }
   };
-  
 
   return (
     <div className="min-h-md flex items-center justify-center bg-gray-50">
-      <div className="bg-white p-8 rounded-lg  max-w-6xl w-full shadow-blue">
+      <div className="bg-white p-8 rounded-lg max-w-6xl w-full shadow-blue">
         <h1 className="text-2xl font-bold mb-6 text-center text-indigo-600">
           Add New School
         </h1>
-        <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
+        <form onSubmit={handleSubmit} encType="multipart/form-data">
+          
           {/* School Name */}
           <div className="mb-4">
             <label
@@ -73,185 +103,124 @@ const page = () => {
               type="text"
               id="name"
               name="name"
-              className={`mt-1 p-2 w-full border rounded-md ${
-                errors.name
-                  ? "border-red-500"
-                  : "border-gray-300 focus:border-indigo-500"
-              }`}
-              {...register("name", { required: true })}
+              value={formData.name}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md border-gray-300 focus:border-indigo-500"
             />
-            {errors.name && (
-              <p className="text-red-500 mt-1">{errors.name.message}</p>
-            )}
           </div>
 
-          {/* Address */}
+          {/* ... (other form fields) ... */}
           <div className="mb-4">
             <label
-              htmlFor="address"
+              htmlFor="addrees"
               className="block text-sm font-medium text-gray-600"
             >
-              Address
+              School Address
             </label>
             <input
               type="text"
               id="address"
               name="address"
-              className={`mt-1 p-2 w-full border rounded-md ${
-                errors.address
-                  ? "border-red-500"
-                  : "border-gray-300 focus:border-indigo-500"
-              }`}
-              {...register("address", { required: true })}
+              value={formData.address}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md border-gray-300 focus:border-indigo-500"
             />
-            {errors.address && (
-              <p className="text-red-500 mt-1">{errors.address.message}</p>
-            )}
           </div>
 
-          {/* City */}
           <div className="mb-4">
             <label
               htmlFor="city"
               className="block text-sm font-medium text-gray-600"
             >
-              City
+              School city
             </label>
             <input
               type="text"
               id="city"
               name="city"
-              className={`mt-1 p-2 w-full border rounded-md ${
-                errors.city
-                  ? "border-red-500"
-                  : "border-gray-300 focus:border-indigo-500"
-              }`}
-              {...register("city", { required: true })}
+              value={formData.city}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md border-gray-300 focus:border-indigo-500"
             />
-            {errors.city && (
-              <p className="text-red-500 mt-1">{errors.city.message}</p>
-            )}
           </div>
 
-          {/* State */}
           <div className="mb-4">
             <label
               htmlFor="state"
               className="block text-sm font-medium text-gray-600"
             >
-              State
+              School state
             </label>
             <input
               type="text"
               id="state"
               name="state"
-              className={`mt-1 p-2 w-full border rounded-md ${
-                errors.state
-                  ? "border-red-500"
-                  : "border-gray-300 focus:border-indigo-500"
-              }`}
-              {...register("state", { required: true })}
+              value={formData.state}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md border-gray-300 focus:border-indigo-500"
             />
-            {errors.state && (
-              <p className="text-red-500 mt-1">{errors.state.message}</p>
-            )}
           </div>
 
-          {/* Contact Number */}
           <div className="mb-4">
             <label
               htmlFor="contact"
               className="block text-sm font-medium text-gray-600"
             >
-              Contact Number
+              School contact
             </label>
             <input
               type="text"
               id="contact"
               name="contact"
-              className={`mt-1 p-2 w-full border rounded-md ${
-                errors.contact
-                  ? "border-red-500"
-                  : "border-gray-300 focus:border-indigo-500"
-              }`}
-              {...register("contact", {
-                pattern: {
-                  value: /^[0-9]{10}$/,
-                  message:
-                    "Invalid contact number. Please enter a 10-digit number.",
-                },
-              })}
+              value={formData.contact}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md border-gray-300 focus:border-indigo-500"
             />
-            {errors.contact && (
-              <p className="text-red-500 mt-1">{errors.contact.message}</p>
-            )}
           </div>
 
-          {/* Email */}
           <div className="mb-4">
             <label
               htmlFor="email"
               className="block text-sm font-medium text-gray-600"
             >
-              Email
+              School email
             </label>
             <input
               type="text"
               id="email"
               name="email"
-              className={`mt-1 p-2 w-full border rounded-md ${
-                errors.email
-                  ? "border-red-500"
-                  : "border-gray-300 focus:border-indigo-500"
-              }`}
-              {...register("email", {
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "Enter a valid email address",
-                },
-              })}
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md border-gray-300 focus:border-indigo-500"
             />
-            {errors.email && (
-              <p className="text-red-500 mt-1">{errors.email.message}</p>
-            )}
           </div>
 
           {/* Image Upload (File Input) */}
           <div className="mb-4">
             <label
-              htmlFor="email"
+              htmlFor="image"
               className="block text-sm font-medium text-gray-600"
             >
               Add School Image
             </label>
             <input
               type="file"
-              {...register("picture", {
-                required: "Image is required",
-                validate: {
-                  validFileType: (value) => {
-                    const allowedFileTypes = [".jpg", ".jpeg", ".png"];
-                    const fileExtension = value[0].name
-                      .split(".")
-                      .pop()
-                      .toLowerCase();
-                    return allowedFileTypes.includes(`.${fileExtension}`);
-                  },
-                },
-              })}
+              name="picture"
+              onChange={handleChange}
               accept=".jpg, .jpeg, .png"
             />
-            {errors.picture && (
-              <p className="text-red-500 mt-1">{errors.picture.message}</p>
-            )}
           </div>
+
+          {/* ... (other form fields) ... */}
+
           {/* Submit Button */}
           <div className="text-center">
             <button
               type="submit"
               className="bg-indigo-500 text-white py-2 px-4 rounded-full font-bold hover:bg-indigo-600 focus:outline-none focus:ring focus:border-indigo-300"
+              disabled={loading}
             >
-              {loading && loading ? "Please wait" : "Add School"}
+              {loading ? 'Please wait' : 'Add School'}
             </button>
           </div>
         </form>
@@ -261,3 +230,4 @@ const page = () => {
 };
 
 export default page;
+
