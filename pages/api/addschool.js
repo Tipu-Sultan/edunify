91bdc255f4f74ffc0c1e5bd5739d9af1827bbc50
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-11-25 02:51:50 +0530

    ok binn

--- pages/api/addschool.js
+++ pages/api/addschool.js
@@ -1,15 +1,24 @@
 import { connectDB } from '../../utils/db';
 import {School} from '../../models/school';
 import multer from 'multer';
+import fs from 'fs';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public/images"); // Uploads folder where files will be stored
+    const uploadPath = 'public/images';
+
+    // Create the directory if it doesn't exist
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname); // Unique filename
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
+
 
 const upload = multer({ storage });
 
