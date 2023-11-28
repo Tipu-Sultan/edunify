e78268ef2354b1a76740304dcfda5dc026943b16
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-11-28 22:41:58 +0530

    rechecked

--- app/addschools/page.jsx
+++ app/addschools/page.jsx
@@ -34,7 +34,7 @@ const page = () => {
     setLoading(true);
   
     try {
-      const response = await axios.post("http://myblogger.000.pe/addschool.php", formData);
+      const response = await axios.post("https://myblogger.000.pe/addschool.php", formData);
   
       if (response.status === 201) {
         const result = response.data;
