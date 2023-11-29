2ce97e8483303793b46e3c1dafc66de3a02df273
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-11-29 22:48:45 +0530

    rechange for env

--- pages/api/addschool.js
+++ pages/api/addschool.js
@@ -5,8 +5,21 @@ import { connectDB } from '../../utils/db';
 import {School} from '../../models/school';
 const storage = new Storage({
   projectId: process.env.PROJECT_KEY_ID,
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  keyFilename: {
+    type: process.env.GOOGLE_CLOUD_TYPE,
+    projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+    privateKeyId: process.env.GOOGLE_CLOUD_PRIVATE_KEY_ID,
+    privateKey: process.env.GOOGLE_CLOUD_PRIVATE_KEY,
+    clientEmail: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+    clientId: process.env.GOOGLE_CLOUD_CLIENT_ID,
+    authUri: process.env.GOOGLE_CLOUD_AUTH_URI,
+    tokenUri: process.env.GOOGLE_CLOUD_TOKEN_URI,
+    authProviderX509CertUrl: process.env.GOOGLE_CLOUD_AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.GOOGLE_CLOUD_CLIENT_X509_CERT_URL,
+    universeDomain: process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN,
+  },
 });
+
 
 const bucket = storage.bucket('edunify');
 
