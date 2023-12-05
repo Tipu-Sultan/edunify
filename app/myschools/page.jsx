062c25c73cf47c776a60d2dada52d81b3073df92
Tipu Sultan <63140791+Tipu-Sultan@users.noreply.github.com>
2023-12-05 11:21:44 +0530

    fix bugs

--- app/myschools/page.jsx
+++ app/myschools/page.jsx
@@ -1,11 +1,87 @@
-import React from 'react'
-
+'use client'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const page = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const [schools, setSchools] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default page
+  useEffect(() => {
+    const fetchSchools = async () => {
+      try {
+        const response = await axios.get('/api/allschool');
+        if (response.status === 200) {
+          setSchools(response.data);
+        } else {
+          console.error('Failed to fetch schools:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching schools:', error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchSchools();
+  }, []);
+
+  const handleDeleteSchool = async (id) => {
+    try {
+      const response = await axios.delete(`/api/allschool/${id}`);
+      if (response.status === 200) {
+        // If deletion is successful, refresh the school list
+        fetchSchools();
+      } else {
+        console.error('Failed to delete school:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error deleting school:', error.message);
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <div className="relative">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-indigo-500 mx-auto"></div>
+            <p className="text-gray-600 absolute bottom-0 mb-4 w-full text-center font-bold">
+              Please wait a couple of minutes.
+            </p>
+          </div>
+        </div>
+      ) : schools.length > 0 ? (
+        <div className="flex items-center justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {schools.map((school, i) => (
+              <SchoolCard key={i} school={school} onDelete={() => handleDeleteSchool(school._id)} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-screen">
+          <p className="text-red-500 font-bold text-center">
+            Oops! Schools are not available at the moment.
+          </p>
+        </div>
+      )}
+    </>
+  );
+
+
+};
+
+const SchoolCard = ({ school,onDelete  }) => {
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md overflow-hidden hover:shadow-xl transition duration-300 delay-150">
+      <h2 className="text-xl font-bold mb-2">{school.name}</h2>
+      <button onClick={onDelete} className="bg-indigo-500 text-white py-2 px-4 rounded-full font-bold hover:bg-indigo-600 focus:outline-none focus:ring focus:border-indigo-300">
+        Apply Now
+      </button>
+    </div>
+  );
+};
+
+
+
+
+export default page;
